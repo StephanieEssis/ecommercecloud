d357ed07e2dc40712851ae03af7dceb5c684f290
stephanieessis@gmail.com <stephanieessis@gmail.com>
2025-04-16 17:53:31 +0000

    fix:product

--- server/server.js
+++ server/server.js
@@ -55,7 +55,8 @@ const seedProducts = async () => {
 // ✅ Connexion à MongoDB puis lancement du serveur
 connectDB().then(() => {
     seedProducts(); // On seed une fois la connexion établie
-    const PORT = process.env.PORT || 5000;
+    // const PORT = process.env.PORT || 5000;
+    const PORT = 5000;
     app.listen(PORT, () => {
         console.log(`🚀 Server running on port ${PORT}`);
     });
